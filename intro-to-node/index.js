372682baafa6e52b62a99c61d6ac2840d2ed0a2c
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-08 12:30:32 +0100

    superVillain

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -8,7 +8,11 @@
 
 
 const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
 
 var mySuperheroName = superheroes.random();
+var mySuperVillain = supervillains.random();
 
 console.log(mySuperheroName);
+console.log(mySuperVillain);
