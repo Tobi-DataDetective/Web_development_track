766173e0bfc8d3929e14c9913b4574ce8b1503e2
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-08 12:24:06 +0100

    superheroes

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,5 +1,14 @@
 //jshint esversion:6
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copFileSync("file.txt", "file2.txt");
+// fs.copyFileSync("file.txt", "file2.txt");
+
+// // console.log("Hello world");
+
+
+const superheroes = require('superheroes');
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
