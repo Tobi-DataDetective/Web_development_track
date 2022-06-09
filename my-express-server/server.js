751f7bc8fbacf716c395d16ed2d68844164b294c
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-09 18:34:01 +0100

    calculator

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,7 +1,26 @@
 // jshint esversion:6
 
 const express = require('express');
+
 const app = express();
+
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello</h1>");
+});
+
+app.get("/contact", function(req, res) {
+    res.send("<h1>Contact me at</h1>");
+});
+
+app.get("/about", function(req, res) {
+    res.send("<h1>I am an incoming Web Developer</h1>");
+});
+
+app.get("/hobbies", function(req, res) {
+    res.send("<ul><li>analytics</li><li>Code</li></ul>");
+});
+
 
 app.listen(3000, function() {
     console.log("server started on port 3000")
