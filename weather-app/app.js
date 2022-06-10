696f7843e84f7d5460bd6bf167cde897f2e9684a
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-10 12:42:07 +0100

    temp and descrp

--- weather-app/app.js
+++ weather-app/app.js
@@ -10,13 +10,44 @@ app.get("/", function(req, res) {
     // connecting to api
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=a37f7763c738ef9868466660b464c130&units=metric";
     https.get(url, function(response) {
-        console.log(response);
+        console.log(response.statusCode); //returning the statusCode
+
+        response.on("data", function(data) {
+            //console.log(data) //returning some kind of hexadecimal data
+            const weatherData = JSON.parse(data) // converting the hexadecimal data to a JSON format
+            console.log(weatherData);
+
+            // tapping into the json file to get the temperature
+            const temp = weatherData.main.temp;
+            console.log(temp);
+
+            // using the JSON viewer in chrome to print out the weather description
+            const descr = weatherData.weather[0].description;
+            console.log(descr);
+
+            // sending or displaying the result we want the client to see
+            res.write("The weather is currently " + descr);
+            res.write("The temperature in Paris is " + temp + "degrees");
+            res.send()
+
+            // trying to revearse the case, from JSON to hex
+            const object = {
+                name: "tobi",
+                age: "25"
+            }
+            console.log(JSON.stringify(object));
+
+
+        });
     });
 
 
 
-    // dusplaying an output
-    res.send("Server is up and running");
+
+
+
+    // displaying an output
+    // res.send("Server is up and running");
 });
 
 
