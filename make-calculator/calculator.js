4978542dcb43a07badb45a512e91f2fd20b4eecd
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-10 09:06:51 +0100

    ibm calculator

--- make-calculator/calculator.js
+++ make-calculator/calculator.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-Parser'); //Allows for the parsing of values th
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//routing to home directory
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
@@ -23,6 +25,21 @@ app.post("/", function(req, res) {
     res.send("The result of the calculation is " + result);
 });
 
+// Routing to bmicalc directory
+app.get("/bmicalc", function(req, res) {
+    res.sendFile(__dirname + "/bmicalc.html");
+});
+
+// passing a get to handle the post attribute in the html 
+app.post("/bmicalc", function(req, res) {
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (weight * height);
+
+    res.send("Your BMI is " + bmi);
+});
 
 
 app.listen(3000, function() {
