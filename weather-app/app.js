11370faafd952c61a6992672e46c99bd11a9f968
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-10 15:02:08 +0100

    weather project completed

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,31 @@
 // importing necessary libraries
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser")
 
 
 const app = express(); //initializing the express library
 
+app.use(bodyParser.urlencoded({ extended: true })); // initializing the body parser which allows to seive throught the html file
+
 app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
 
     // connecting to api
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=a37f7763c738ef9868466660b464c130&units=metric";
+
+    const query = req.body.cityName; // passing in the input value from the html
+    const appid = "a37f7763c738ef9868466660b464c130";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appid + "&units=" + unit;
+
     https.get(url, function(response) {
         console.log(response.statusCode); //returning the statusCode
 
         response.on("data", function(data) {
+
             //console.log(data) //returning some kind of hexadecimal data
             const weatherData = JSON.parse(data) // converting the hexadecimal data to a JSON format
             console.log(weatherData);
@@ -25,10 +38,15 @@ app.get("/", function(req, res) {
             const descr = weatherData.weather[0].description;
             console.log(descr);
 
+            // including icon 
+            const icon = weatherData.weather[0].icon;
+            const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
             // sending or displaying the result we want the client to see
-            res.write("The weather is currently " + descr);
-            res.write("The temperature in Paris is " + temp + "degrees");
-            res.send()
+            res.write("<p>The weather is currently " + descr + "</p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees. </h1>");
+            res.write("<img src=" + imgURL + ">");
+            res.send();
 
             // trying to revearse the case, from JSON to hex
             const object = {
@@ -48,6 +66,9 @@ app.get("/", function(req, res) {
 
     // displaying an output
     // res.send("Server is up and running");
+
+
+    // console.log("Post received");
 });
 
 
